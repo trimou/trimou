e6436ab33e02454b5b0d7f1936bf626ab0b20986
Martin Kouba <mkouba@redhat.com>
2013-05-21 21:40:09 +0200

    PrettyTimeResolver - minor improvements - configurable matching name - handle Long and Calendar context objects

--- extensions/prettytime/src/main/java/org/trimou/prettytime/resolver/PrettyTimeResolver.java
+++ extensions/prettytime/src/main/java/org/trimou/prettytime/resolver/PrettyTimeResolver.java
@@ -2,11 +2,18 @@ package org.trimou.prettytime.resolver;
 
 import static org.trimou.engine.priority.Priorities.after;
 
+import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.ocpsoft.prettytime.PrettyTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.trimou.engine.config.Configuration;
+import org.trimou.engine.config.ConfigurationKey;
+import org.trimou.engine.config.SimpleConfigurationKey;
 import org.trimou.engine.resolver.ArrayIndexResolver;
 import org.trimou.engine.resolver.i18n.LocaleAwareResolver;
 
@@ -21,9 +28,15 @@ import com.google.common.cache.LoadingCache;
  */
 public class PrettyTimeResolver extends LocaleAwareResolver {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(PrettyTimeResolver.class);
+
 	public static final int PRETTY_TIME_RESOLVER_PRIORITY = after(ArrayIndexResolver.ARRAY_RESOLVER_PRIORITY);
 
-	private static final String MATCH_NAME = "prettyTime";
+	public static final ConfigurationKey MATCH_NAME_KEY = new SimpleConfigurationKey(
+			PrettyTimeResolver.class.getName() + ".matchName", "prettyTime");
+
+	private String matchName;
 
 	/**
 	 * Lazy loading cache of PrettyTime instances
@@ -33,15 +46,18 @@ public class PrettyTimeResolver extends LocaleAwareResolver {
 	@Override
 	public Object resolve(Object contextObject, String name) {
 
-		if (contextObject == null || !(contextObject instanceof Date)) {
+		if (contextObject == null || !matchName.equals(name)) {
 			return null;
 		}
 
-		if (MATCH_NAME.equals(name)) {
-			return prettyTimeCache.getUnchecked(getCurrentLocale()).format(
-					(Date) contextObject);
+		Date formattableObject = getFormattableObject(contextObject);
+
+		if (formattableObject == null) {
+			return null;
 		}
-		return null;
+
+		return prettyTimeCache.getUnchecked(getCurrentLocale()).format(
+				formattableObject);
 	}
 
 	@Override
@@ -50,8 +66,15 @@ public class PrettyTimeResolver extends LocaleAwareResolver {
 	}
 
 	@Override
+	public List<ConfigurationKey> getConfigurationKeys() {
+		return Collections.singletonList(MATCH_NAME_KEY);
+	}
+
+	@Override
 	public void init(Configuration configuration) {
 		super.init(configuration);
+
+		matchName = configuration.getStringPropertyValue(MATCH_NAME_KEY);
 		prettyTimeCache = CacheBuilder.newBuilder().maximumSize(10)
 				.build(new CacheLoader<Locale, PrettyTime>() {
 
@@ -60,6 +83,18 @@ public class PrettyTimeResolver extends LocaleAwareResolver {
 						return new PrettyTime(locale);
 					}
 				});
+		logger.info("Initialized [matchName: {}]", matchName);
+	}
+
+	private Date getFormattableObject(Object contextObject) {
+		if (contextObject instanceof Date) {
+			return (Date) contextObject;
+		} else if (contextObject instanceof Calendar) {
+			return ((Calendar) contextObject).getTime();
+		} else if (contextObject instanceof Long) {
+			return new Date((Long) contextObject);
+		}
+		return null;
 	}
 
 }
--- extensions/prettytime/src/test/java/org/trimou/prettytime/resolver/PrettyTimeResolverTest.java
+++ extensions/prettytime/src/test/java/org/trimou/prettytime/resolver/PrettyTimeResolverTest.java
@@ -1,8 +1,10 @@
 package org.trimou.prettytime.resolver;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -11,6 +13,7 @@ import org.ocpsoft.prettytime.i18n.Resources_en;
 import org.trimou.engine.MustacheEngine;
 import org.trimou.engine.MustacheEngineBuilder;
 import org.trimou.engine.locale.LocaleSupport;
+import org.trimou.engine.resolver.MapResolver;
 
 import com.google.common.collect.ImmutableMap;
 
@@ -21,28 +24,58 @@ import com.google.common.collect.ImmutableMap;
 public class PrettyTimeResolverTest {
 
 	@Test
-	public void testOnlyDateIsResolved() {
+	public void testFormattableObjectsResolved() {
+
 		PrettyTimeResolver resolver = new PrettyTimeResolver();
-		assertNull(resolver.resolve(null, "prettyTime"));
-		assertNull(resolver.resolve("foo", "prettyTime"));
-	}
 
-	@Test
-	public void testInterpolation() {
-
-		MustacheEngine engine = MustacheEngineBuilder.newBuilder()
+		// Just to init the resolver
+		MustacheEngineBuilder.newBuilder().omitServiceLoaderResolvers()
 				.setLocaleSupport(new LocaleSupport() {
 
 					@Override
 					public Locale getCurrentLocale() {
 						return Locale.ENGLISH;
 					}
-				}).addResolver(new PrettyTimeResolver()).build();
+				}).addResolver(resolver).build();
 
+		assertNull(resolver.resolve(null, "prettyTime"));
+		assertNull(resolver.resolve("foo", "prettyTime"));
+		assertNotNull(resolver.resolve(new Date(), "prettyTime"));
+		assertNotNull(resolver.resolve(10000l, "prettyTime"));
+		assertNotNull(resolver.resolve(Calendar.getInstance(), "prettyTime"));
+	}
+
+	@Test
+	public void testInterpolation() {
+
+		MustacheEngine engine = MustacheEngineBuilder.newBuilder()
+				.omitServiceLoaderResolvers()
+				.setLocaleSupport(new LocaleSupport() {
+
+					@Override
+					public Locale getCurrentLocale() {
+						return Locale.ENGLISH;
+					}
+				})
+				.addResolver(new MapResolver())
+				.addResolver(new PrettyTimeResolver()).build();
+
+		String expected = new Resources_en().getString("JustNowPastPrefix");
+		Calendar now = Calendar.getInstance();
+
+		assertEquals(expected,
+				engine.compileMustache("pretty_cal", "{{now.prettyTime}}")
+						.render(ImmutableMap.<String, Object> of("now", now)));
 		assertEquals(
-				new Resources_en().getString("JustNowPastPrefix"),
-				engine.compileMustache("pretty", "{{now.prettyTime}}").render(
-						ImmutableMap.<String, Object> of("now", new Date())));
+				expected,
+				engine.compileMustache("pretty_date", "{{now.prettyTime}}")
+						.render(ImmutableMap.<String, Object> of("now",
+								now.getTime())));
+		assertEquals(
+				expected,
+				engine.compileMustache("pretty_long", "{{now.prettyTime}}")
+						.render(ImmutableMap.<String, Object> of("now",
+								now.getTimeInMillis())));
 	}
 
 }
