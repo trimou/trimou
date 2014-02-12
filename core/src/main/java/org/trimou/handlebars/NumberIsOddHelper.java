5091da6c42f91d451115263ea4d2bceadcd4fe34
Martin Kouba <mkouba@redhat.com>
2014-02-12 12:03:31 +0100

    Add abstract NumberMatchingHelper to unify some logic

--- core/src/main/java/org/trimou/handlebars/NumberIsOddHelper.java
+++ core/src/main/java/org/trimou/handlebars/NumberIsOddHelper.java
@@ -1,7 +1,5 @@
 package org.trimou.handlebars;
 
-import org.trimou.exception.MustacheException;
-import org.trimou.exception.MustacheProblem;
 
 /**
  * <code>
@@ -20,40 +18,10 @@ import org.trimou.exception.MustacheProblem;
  *
  * @author Martin Kouba
  */
-public class NumberIsOddHelper extends BasicHelper {
+public class NumberIsOddHelper extends NumberMatchingHelper {
 
     @Override
-    public void execute(Options options) {
-
-        Object param = options.getParameters().get(0);
-
-        if (param instanceof Number) {
-
-            if (isOdd((Number) param)) {
-                if (isSection(options)) {
-                    options.fn();
-                } else {
-                    if (options.getParameters().size() > 1) {
-                        append(options, options.getParameters().get(1)
-                                .toString());
-                    } else {
-                        throw new MustacheException(
-                                MustacheProblem.RENDER_HELPER_INVALID_OPTIONS,
-                                "Invalid number of params for variable tag [params: %s, template: %s, line: %s]",
-                                options.getParameters().size(), options
-                                        .getTagInfo().getTemplateName(),
-                                options.getTagInfo().getLine());
-                    }
-                }
-            } else {
-                if (isVariable(options) && options.getParameters().size() > 2) {
-                    append(options, options.getParameters().get(2).toString());
-                }
-            }
-        }
-    }
-
-    private boolean isOdd(Number value) {
+    protected boolean isMatching(Number value) {
         return value.intValue() % 2 != 0;
     }
 
---
+++ core/src/main/java/org/trimou/handlebars/NumberMatchingHelper.java
@@ -0,0 +1,46 @@
+package org.trimou.handlebars;
+
+import org.trimou.exception.MustacheException;
+import org.trimou.exception.MustacheProblem;
+
+/**
+ * An abstract helper for matching {@link Number} values.
+ *
+ * @author Martin Kouba
+ */
+public abstract class NumberMatchingHelper extends BasicHelper {
+
+    @Override
+    public void execute(Options options) {
+
+        Object param = options.getParameters().get(0);
+
+        if (param instanceof Number) {
+
+            if (isMatching((Number) param)) {
+                if (isSection(options)) {
+                    options.fn();
+                } else {
+                    if (options.getParameters().size() > 1) {
+                        append(options, options.getParameters().get(1)
+                                .toString());
+                    } else {
+                        throw new MustacheException(
+                                MustacheProblem.RENDER_HELPER_INVALID_OPTIONS,
+                                "Invalid number of params for variable tag [params: %s, template: %s, line: %s]",
+                                options.getParameters().size(), options
+                                        .getTagInfo().getTemplateName(),
+                                options.getTagInfo().getLine());
+                    }
+                }
+            } else {
+                if (isVariable(options) && options.getParameters().size() > 2) {
+                    append(options, options.getParameters().get(2).toString());
+                }
+            }
+        }
+    }
+
+    protected abstract boolean isMatching(Number value);
+
+}
