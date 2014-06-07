265c94bbb775b567f03c04003fff14d88c1d3a5a
Minkyu Cho <mrnoname@naver.com>
2014-06-07 18:08:21 +0900

    Implement Spring View Resolver and separate with EmbedHeplper Implementation (#29)

---
+++ extensions/spring-mvc/src/main/java/org/springframework/web/servlet/view/trimou/TrimouViewResolver.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2014 Minkyu Cho
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.web.servlet.view.trimou;
+
+import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.UncheckedExecutionException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.AbstractTemplateViewResolver;
+import org.springframework.web.servlet.view.AbstractUrlBasedView;
+import org.springframework.web.servlet.view.trimou.TrimouView;
+import org.trimou.Mustache;
+import org.trimou.engine.MustacheEngine;
+import org.trimou.engine.MustacheEngineBuilder;
+import org.trimou.engine.config.ConfigurationKey;
+import org.trimou.engine.config.EngineConfigurationKey;
+import org.trimou.exception.MustacheException;
+import org.trimou.handlebars.Helper;
+import org.trimou.servlet.locator.ServletContextTemplateLocator;
+
+import javax.servlet.ServletContext;
+import java.util.Map;
+
+/**
+ * @author Minkyu Cho
+ */
+public class TrimouViewResolver extends AbstractTemplateViewResolver implements ViewResolver, InitializingBean {
+	private String fileEncoding = System.getProperty("file.encoding");
+	private boolean handlebarsSupport = true;
+	private boolean debug = false;
+	private boolean preCompile = false;
+	private Map<String, Helper> helpers = Maps.newHashMap();
+	private MustacheEngine engine;
+
+	public TrimouViewResolver() {
+		setViewClass(TrimouView.class);
+	}
+
+	@Override
+	protected AbstractUrlBasedView buildView(String viewName) throws Exception {
+		TrimouView view = (TrimouView) super.buildView(viewName);
+		try {
+			Mustache template = engine.getMustache(viewName);
+			view.setTemplate(template);
+			return view;
+		} catch (Exception e) {
+			throw new MustacheException(view.getUrl() + " : "  + e.getMessage());
+		}
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		final int PRIORITY = 1;
+		engine = MustacheEngineBuilder
+				.newBuilder()
+				.setProperty(EngineConfigurationKey.TEMPLATE_CACHE_ENABLED, isCache())
+				.setProperty(EngineConfigurationKey.DEFAULT_FILE_ENCODING, getFileEncoding())
+				.setProperty(EngineConfigurationKey.HANDLEBARS_SUPPORT_ENABLED, isHandlebarsSupport())
+				.setProperty(EngineConfigurationKey.DEBUG_MODE, isDebug())
+				.setProperty(EngineConfigurationKey.PRECOMPILE_ALL_TEMPLATES, isPreCompile())
+				.registerHelpers(helpers)
+				.addTemplateLocator(new ServletContextTemplateLocator(PRIORITY, getPrefix(), getSuffix(), getServletContext()))
+				.build();
+	}
+
+	@Override
+	protected Class<?> requiredViewClass() {
+		return TrimouView.class;
+	}
+
+	public String getFileEncoding() {
+		return fileEncoding;
+	}
+
+	public void setFileEncoding(String fileEncoding) {
+		this.fileEncoding = fileEncoding;
+	}
+
+	public boolean isHandlebarsSupport() {
+		return handlebarsSupport;
+	}
+
+	public void setHandlebarsSupport(boolean handlebarsSupport) {
+		this.handlebarsSupport = handlebarsSupport;
+	}
+
+	public boolean isDebug() {
+		return debug;
+	}
+
+	public void setDebug(boolean debug) {
+		this.debug = debug;
+	}
+
+	public boolean isPreCompile() {
+		return preCompile;
+	}
+
+	public void setPreCompile(boolean preCompile) {
+		this.preCompile = preCompile;
+	}
+
+	public Map<String, Helper> getHelpers() {
+		return helpers;
+	}
+
+	public void setHelpers(Map<String, Helper> helpers) {
+		this.helpers = helpers;
+	}
+
+	public MustacheEngine getEngine() {
+		return engine;
+	}
+
+	public void setEngine(MustacheEngine engine) {
+		this.engine = engine;
+	}
+}
---
+++ extensions/spring-mvc/src/test/java/org/springframework/web/servlet/view/trimou/TrimouViewResolverTest.java
@@ -0,0 +1,80 @@
+package org.springframework.web.servlet.view.trimou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.web.servlet.view.AbstractUrlBasedView;
+import org.springframework.web.servlet.view.trimou.TrimouViewResolver;
+import org.trimou.Mustache;
+import org.trimou.engine.MustacheEngine;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ * @author Minkyu Cho
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TrimouViewResolverTest {
+
+	@Mock
+	private Mustache mustache;
+	@Mock
+	private MustacheEngine engine;
+	@Mock
+	private ServletContext servletContext;
+
+	/**
+	 * A basic test where no prefix is used.
+	 */
+	@Test
+	public void resolvesViewWithoutPrefix() throws Exception {
+		final String viewPath = "top-level.mustache";
+		when(engine.getMustache(viewPath)).thenReturn(mustache);
+
+		TrimouViewResolver sut = new TrimouViewResolver();
+		sut.setServletContext(servletContext);
+		sut.afterPropertiesSet();
+		sut.setEngine(engine);
+
+		AbstractUrlBasedView view = sut.buildView(viewPath);
+		assertThat(view, is(notNullValue()));
+	}
+
+	/**
+	 * Ensure the prefix is passed on to the template loader
+	 * and that the template loader is called with a fully 
+	 * resolved view path.
+	 */
+	@Test
+	public void resolvesViewWithPrefix() throws Exception {
+		final String viewPath = "/WEB-INF/views/";
+		final String viewName = "hello.mustache";
+
+		when(engine.getMustache(viewPath + viewName)).thenReturn(mustache);
+
+		TrimouViewResolver sut = new TrimouViewResolver();
+		sut.setServletContext(servletContext);
+		sut.setPrefix(viewPath);
+		sut.afterPropertiesSet();
+		sut.setEngine(engine);
+
+		AbstractUrlBasedView view = sut.buildView(viewName);
+		assertThat(view, is(notNullValue()));
+	}
+}
