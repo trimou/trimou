8fa1584fbb2fe9faec917b234cdcff4929f43e68
Martin Kouba <mkouba@redhat.com>
2016-04-22 16:05:06 +0200

    AbstractContainerSegment - avoid unnecessary iterations for single child

--- core/src/main/java/org/trimou/engine/segment/AbstractContainerSegment.java
+++ core/src/main/java/org/trimou/engine/segment/AbstractContainerSegment.java
@@ -15,6 +15,7 @@
  */
 package org.trimou.engine.segment;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -31,6 +32,10 @@ import org.trimou.util.ImmutableList.ImmutableListBuilder;
 abstract class AbstractContainerSegment extends AbstractSegment
         implements ContainerSegment {
 
+    // Sometimes there is only one segment in the container - we use this
+    // reference to avoid unnecessary iterations
+    private final Segment singleton;
+
     private final List<Segment> segments;
 
     /**
@@ -43,18 +48,23 @@ abstract class AbstractContainerSegment extends AbstractSegment
             List<Segment> segments) {
         super(text, origin);
         this.segments = segments;
+        this.singleton = segments.size() == 1 ? segments.get(0) : null;
     }
 
     public Appendable execute(Appendable appendable, ExecutionContext context) {
-        for (Segment segment : segments) {
-            appendable = segment.execute(appendable, context);
+        if (singleton != null) {
+            appendable = singleton.execute(appendable, context);
+        } else {
+            for (Segment segment : segments) {
+                appendable = segment.execute(appendable, context);
+            }
         }
         return appendable;
     }
 
     @Override
     public Iterator<Segment> iterator() {
-        return getSegments().iterator();
+        return segments.iterator();
     }
 
     public List<Segment> getSegments() {
@@ -64,7 +74,7 @@ abstract class AbstractContainerSegment extends AbstractSegment
     public int getSegmentsSize(boolean recursive) {
         if (recursive) {
             int count = 0;
-            for (Segment segment : this) {
+            for (Segment segment : segments) {
                 count++;
                 if (segment instanceof ContainerSegment) {
                     count += ((ContainerSegment) segment).getSegmentsSize(true);
@@ -76,6 +86,9 @@ abstract class AbstractContainerSegment extends AbstractSegment
     }
 
     public String getContentLiteralBlock() {
+        if (singleton != null) {
+            return singleton.getLiteralBlock();
+        }
         StringBuilder literal = new StringBuilder();
         for (Segment segment : segments) {
             literal.append(segment.getLiteralBlock());
@@ -90,6 +103,9 @@ abstract class AbstractContainerSegment extends AbstractSegment
 
     @Override
     protected List<MustacheTagInfo> getDirectChildTags() {
+        if (singleton != null) {
+            return Collections.singletonList(singleton.getTagInfo());
+        }
         ImmutableListBuilder<MustacheTagInfo> builder = ImmutableList.builder();
         for (Segment segment : segments) {
             if (segment.getType().getTagType() != null) {
