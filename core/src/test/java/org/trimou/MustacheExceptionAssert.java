27605d929354c83cddd06143709c9e9afa61d04f
Harald Wellmann <harald.wellmann@gmx.de>
2016-09-24 18:07:11 +0200

    Preserve exception message (#93)

--- core/src/test/java/org/trimou/MustacheExceptionAssert.java
+++ core/src/test/java/org/trimou/MustacheExceptionAssert.java
@@ -12,10 +12,18 @@ import org.trimou.exception.ProblemCode;
 public class MustacheExceptionAssert extends ExceptionAssert<MustacheException> {
 
     private final ProblemCode expectedCode;
+    private final String messageSubstring;
 
     private MustacheExceptionAssert(ProblemCode expectedCode) {
         super(MustacheException.class);
         this.expectedCode = expectedCode;
+        this.messageSubstring = null;
+    }
+
+    private MustacheExceptionAssert(ProblemCode expectedCode, String messageSubstring) {
+        super(MustacheException.class);
+        this.expectedCode = expectedCode;
+        this.messageSubstring = messageSubstring;
     }
 
     @Override
@@ -23,10 +31,17 @@ public class MustacheExceptionAssert extends ExceptionAssert<MustacheException> 
         if (!e.getCode().equals(expectedCode)) {
             fail("Unexpected problem code: " + e.getCode());
         }
+        if (messageSubstring != null && !e.getMessage().contains(messageSubstring)) {
+            fail(String.format("expected message with substring [%s], got [%s]", messageSubstring, e.getMessage()));
+        }
     }
 
     public static MustacheExceptionAssert expect(ProblemCode code) {
         return new MustacheExceptionAssert(code);
+    }
+
+    public static MustacheExceptionAssert expect(ProblemCode code, String messageSubstring) {
+        return new MustacheExceptionAssert(code, messageSubstring);
     }
 
 }
